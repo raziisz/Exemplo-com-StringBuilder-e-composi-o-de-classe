f8c85b140f7119110112ebecfc003c361037e79a
Luiz Felipe <raziel_libertino@hotmail.com>
2019-01-29 17:02:56 -0300

    Finalizado classe Poste

--- TrabPost/Classes/Poste.cs
+++ TrabPost/Classes/Poste.cs
@@ -6,5 +6,30 @@ namespace TrabPost.Classes
 {
     class Poste
     {
+        public DateTime Momento { get; set; }
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public int Likes { get; set; }
+        public List<Comentario> Comentarios { get; set; } = new List<Comentario>();
+
+        public Poste() { }
+
+        public Poste(DateTime momento, string titulo, string conteudo, int likes)
+        {
+            Momento = momento;
+            Titulo = titulo;
+            Conteudo = conteudo;
+            Likes = likes;
+        }
+
+        public void AddComentarios(Comentario comentario)
+        {
+            Comentarios.Add(comentario);
+        }
+
+        public void RemoverComentarios( Comentario comentario)
+        {
+            Comentarios.Remove(comentario);
+        }
     }
 }
