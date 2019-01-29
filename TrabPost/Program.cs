e4694450c26cacb1af3932e6d5b152d6efa84c94
LUIZ FELIPE CARVALHO LIBERTINO <LUIZ.LIBERTINO@ssp.ic.am.gov.br>
2019-01-29 16:49:40 -0300

    Adicionar arquivos de projeto.

---
+++ TrabPost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrabPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
