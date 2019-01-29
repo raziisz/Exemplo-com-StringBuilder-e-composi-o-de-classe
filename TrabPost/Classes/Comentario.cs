d494a0736fc66e86e6ff60412beb486cd189fbdf
Luiz Felipe <raziel_libertino@hotmail.com>
2019-01-29 16:55:40 -0300

    adicionado as classes

---
+++ TrabPost/Classes/Comentario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrabPost.Classes
+{
+    class Comentario
+    {
+        public string Texto { get; set; }
+
+        public Comentario() { }
+
+        public Comentario(string texto)
+        {
+            Texto = texto;
+        }
+    }
+}
